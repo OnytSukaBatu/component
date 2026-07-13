53a6a344e7464c030610aa3fc4925584bab1276a
valenty <valentynoelsan@gmail.com>
2026-07-13 08:40:37 +0700

    [0.0.1+1] Update function

---
+++ lib/function/main_function.dart
@@ -0,0 +1,51 @@
+import 'dart:developer' as dev;
+
+import 'package:main/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+part 'navigation_function.dart';
+part 'overlay_function.dart';
+
+class MainFunction with NavigationFunction, OverlayFunction {
+  static final MainFunction _instance = MainFunction._internal();
+  MainFunction._internal();
+  factory MainFunction() => _instance;
+
+  void log(
+    dynamic log, {
+    Color? color,
+    String? name,
+    bool? bold,
+    bool? faint,
+    bool? italic,
+    bool? underline,
+  }) {
+    name ??= 'log';
+    String logStr = log is String ? log : log.toString();
+
+    List<String> codes = [];
+    if (bold == true) codes.add('1');
+    if (faint == true) codes.add('2');
+    if (italic == true) codes.add('3');
+    if (underline == true) codes.add('4');
+
+    if (color != null) {
+      int red = (color.r * 255.0).round() & 0xff;
+      int green = (color.g * 255.0).round() & 0xff;
+      int blue = (color.b * 255.0).round() & 0xff;
+      codes.add('38;2;$red;$green;$blue');
+    }
+
+    if (codes.isNotEmpty) {
+      String start = '\x1B[${codes.join(';')}m';
+      String end = '\x1B[0m';
+
+      logStr = logStr.split('\n').map((line) => '$start$line$end').join('\n');
+    }
+
+    dev.log(logStr, name: name);
+  }
+}
+
+MainFunction get f => MainFunction._instance;
--- lib/function/overlay_function.dart
+++ lib/function/overlay_function.dart
@@ -1,12 +1,6 @@
-import 'package:component/main.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+part of 'main_function.dart';
 
-class MainFunction with NavigationFunction {
-  static final MainFunction _instance = MainFunction._internal();
-  MainFunction._internal();
-  factory MainFunction() => _instance;
-
+mixin OverlayFunction {
   ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackBar({
     required Widget content,
     SnackBarAction? action,
@@ -264,45 +258,3 @@ class MainFunction with NavigationFunction {
     );
   }
 }
-
-mixin NavigationFunction {
-  void back<T>({T? result}) {
-    BuildContext? context = navigatorKey.currentContext;
-    if (context == null) return;
-
-    Navigator.pop<T>(context, result);
-  }
-
-  Future<T?> to<T>(Widget page) async {
-    BuildContext? context = navigatorKey.currentContext;
-    if (context == null) return null;
-
-    return await Navigator.push<T>(
-      context,
-      MaterialPageRoute(builder: (context) => page),
-    );
-  }
-
-  Future<T?> off<TO, T>(Widget page) async {
-    BuildContext? context = navigatorKey.currentContext;
-    if (context == null) return null;
-
-    return await Navigator.pushReplacement<T, TO>(
-      context,
-      MaterialPageRoute(builder: (context) => page),
-    );
-  }
-
-  Future<T?> offAll<T>(Widget page) async {
-    BuildContext? context = navigatorKey.currentContext;
-    if (context == null) return null;
-
-    return await Navigator.pushAndRemoveUntil<T>(
-      context,
-      MaterialPageRoute(builder: (context) => page),
-      (route) => false,
-    );
-  }
-}
-
-MainFunction get f => MainFunction._instance;
